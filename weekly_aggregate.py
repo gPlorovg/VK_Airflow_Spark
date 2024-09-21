57d31f4113c9b542bc60d1109a9a911912307293
gplorovg <gplorovg@gmail.com>
2024-09-21 18:13:12 +0300

    add SparkSession & raw data, daily data structures

--- weekly_aggregate.py
+++ weekly_aggregate.py
@@ -1,6 +1,9 @@
 from argparse import ArgumentParser
 from datetime import datetime, timedelta
 
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, LongType
+
 parser = ArgumentParser(description="date of start | input path| output path| daily path")
 
 parser.add_argument("--execution_date", help="Date of start weekly aggregate")
@@ -12,3 +15,19 @@ args = parser.parse_args()
 
 start_date = datetime.strptime(args.execution_date, "%Y-%m-%d")
 last_week_daily_files = [(start_date - timedelta(days=i)).strftime("%Y-%m-%d.csv") for i in range(1, 8)]
+
+spark = SparkSession.builder.appName("Weekly user CRUD action counter").getOrCreate()
+
+raw_data_schema = StructType([
+    StructField("email", StringType()),
+    StructField("crud", StringType()),
+    StructField("timestamp", TimestampType()),
+])
+
+daily_data_schema = StructType([
+    StructField("email", StringType()),
+    StructField("create_count", LongType()),
+    StructField("read_count", LongType()),
+    StructField("update_count", LongType()),
+    StructField("delete_count", LongType()),
+])
